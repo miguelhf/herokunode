2872774799b3cef5f87d87730477403127ee2a85
migueleduardo.hernaiz <migueleduardo.hernaiz@bbva.com>
2018-08-17 10:13:51 +0200

    portfolio ready

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Portfolio page'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server up on port ${port}...`);
 });
